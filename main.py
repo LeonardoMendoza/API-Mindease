e5d45faf584032d3ad4ae865895f45eab26545b0
LeonardoMendoza <gadi20_@hotmail.com>
2023-11-13 01:10:29 -0600

    Another with only app run

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-
     return 'Hello, World!'
 
 @app.route('/')
@@ -15,7 +14,6 @@ def status():
 if __name__ == '__main__':
     # Configura la variable de entorno FLASK_ENV
     app.config['FLASK_ENV'] = 'production'
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='mindeaseapi.azurewebsites.net', port=port)
+    app.run()
 
 
