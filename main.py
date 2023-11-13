98c2f13bd60ab276df30cbe00c0e2fe96e6c19db
LeonardoMendoza <gadi20_@hotmail.com>
2023-11-12 19:11:10 -0600

    Removed port
    Lines for local network api has removed

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+
+
