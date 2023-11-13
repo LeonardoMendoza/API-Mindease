761bac98bcda4e4f2d5f3c5449ee658b703544c6
LeonardoMendoza <gadi20_@hotmail.com>
2023-11-12 19:45:03 -0600

    port addeds again
    Another test with specify port

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
+
     return 'Hello, World!'
 
 @app.route('/')
 def status():
     return 'OK'
 
-
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
