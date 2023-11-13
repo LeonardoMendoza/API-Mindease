c74af338168f390809aa1a5f85f0313971b43a67
LeonardoMendoza <gadi20_@hotmail.com>
2023-11-13 01:57:42 -0600

    test hola
    Nueva ruta for fun ^^

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def hello_world():
     return 'Hello, Perla :D!'
@@ -10,6 +11,10 @@ def hello_world():
 @app.route('/')
 def status():
     return 'OK'
+
+@app.route('/hola_choki')
+def status():
+    return 'Kionda pa'
 
 if __name__ == '__main__':
     # Configura la variable de entorno FLASK_ENV
